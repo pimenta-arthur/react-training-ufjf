f6e562f99012a8d3dddb74778cafa8289448ef65
Arthur Pimenta <arituca@gmail.com>
2018-12-12 18:20:59 -0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Feed from './components/Feed';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
+
+  notFoundComponent() {
+    return(
+      <div>
+        Página não encontrada
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <Feed />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Feed} />
+            <Route path="*" component={this.notFoundComponent} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
